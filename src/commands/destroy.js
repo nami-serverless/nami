4c0b2fb10c12ff389c4d39abca69dd736eadb39b
Nick Miller <nick.miller00@gmail.com>
2019-04-06 11:59:59 -0700

    Remove unneccessary files
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -8,10 +8,10 @@ module.exports = async function destroy(resourceName, options, homedir) {
   const preLambda = `${resourceName}PreLambda`;
   const postLambda = `${resourceName}PostLambda`;
 
-  // await stopEC2Instance(resourceName);
-  // await deleteEventSourceMapping(resourceName);
-  // await deleteSQS(resourceName, homedir);
-  // await deleteLambda(preLambda);
-  // await deleteLambda(postLambda);
+  await stopEC2Instance(resourceName);
+  await deleteEventSourceMapping(resourceName);
+  await deleteSQS(resourceName, homedir);
+  await deleteLambda(preLambda);
+  await deleteLambda(postLambda);
   await deleteApiResource(resourceName, homedir);
 };
--- src/util/setupNamiDirAndFiles.js
+++ src/util/setupNamiDirAndFiles.js
@@ -1,4 +1,4 @@
-const { asyncGetCallerIdentity, asyncGetRegions } = require('../aws/awsFunctions');
+const { asyncGetCallerIdentity } = require('../aws/awsFunctions');
 
 const {
   createDirectory,
@@ -6,17 +6,6 @@ const {
   getNamiPath,
   copyEC2SetupScript,
 } = require('./fileUtils');
-
-// fix hard coding by using asyncGetRegions
-const regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
-  'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2',
-  'eu-west-3', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3',
-  'ap-southeast-1', 'ap-southeast-2', 'ap-south-1', 'sa-east-1'];
-
-const startingTemplate = regions.reduce((acc, region) => {
-  acc[region] = {};
-  return acc;
-}, {});
 
 module.exports = async function setupNamiDirAndFiles(roleName, homePath) {
   const accountNumber = (await asyncGetCallerIdentity()).Account;
@@ -36,9 +25,6 @@ module.exports = async function setupNamiDirAndFiles(roleName, homePath) {
     await createDirectory('staging', namiPath);
     await createJSONFile('config', namiPath, configJSON);
     await createJSONFile('resources', namiPath, resourcesJSON);
-    await createJSONFile('lambdas', namiPath, startingTemplate);
-    await createJSONFile('apis', namiPath, startingTemplate);
-    await createJSONFile('dbTables', namiPath, startingTemplate);
     await copyEC2SetupScript(scriptLocation);
     // need config file for SQS?
   } catch (err) {
