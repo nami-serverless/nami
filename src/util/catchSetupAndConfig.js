5a1338af3cfbd7a121f1b129675559d2a98864ce
Wendy Kuhn <wendy@wendykuhn.io>
2019-04-09 17:24:07 -0500

    only initialize on deploy command, lint
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/util/catchSetupAndConfig.js
+++ src/util/catchSetupAndConfig.js
@@ -1,36 +1,21 @@
 // const getUserDefaults = require('./getUserDefaults');
 const init = require('./init');
+
 const namiRole = 'namiRole';
 
 const {
-  readConfig,
   getNamiPath,
   exists,
 } = require('./fileUtils');
-
-const commands = [
-  'create',
-  'deploy',
-  'redeploy',
-  'list',
-  'get',
-  'delete',
-  'config',
-  'dbtable',
-  'help',
-  'version',
-];
-const commandIsNotValid = command => !commands.includes(command);
 
 module.exports = async function catchSetupAndConfig(homedir, command) {
   const namiPath = await getNamiPath(homedir);
   const namiDirExists = await exists(namiPath);
 
-  if (!namiDirExists) {
+  if (!namiDirExists && command === 'deploy') {
     const isInitialized = await init(namiRole, homedir);
 
-    // don't continue if init incomplete, don't config twice
-    if (!isInitialized || command === 'config') return false;
+    if (!isInitialized) return false;
   }
 
   return true;
