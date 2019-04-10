9921db685c17f911cc4f06c756f2d09d3ee9c427
Sachin Chandy <sachinmchandy@gmail.com>
2019-04-10 20:29:20 +0100

    user prompt for directory location
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/commands/create.js
+++ src/commands/create.js
@@ -10,8 +10,7 @@ module.exports = async function create(resourceName, homedir) {
   let postLambdaTemplate = await getTemplate('postLambda', false, resourceName);
 
   await writeTemplateLocally(preLambdaName, preLambdaTemplate);
-  namiLog('preLambda template is in your directory');
   await writeTemplateLocally(postLambdaName, postLambdaTemplate);
-  namiLog('postLambda template is in your directory');
-  // namiLog('your lambda templates, ${preLambdaName} and ${postLambdaName} are in process.cwd ')
+  namiLog(`Your lambda templates, ${preLambdaName} and ${postLambdaName} are in ${process.cwd()}`);
+  namiLog('Please make any modifications to them before calling deploy');
 };
