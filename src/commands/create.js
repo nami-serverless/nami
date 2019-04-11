5230b8682923b493e0a34be6849844383db84b5e
Nick Miller <nick.miller00@gmail.com>
2019-04-11 12:40:43 -0700

    Handle checking for existing directory in create command
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,16 +1,30 @@
-const { writeTemplateLocally } = require('./../util/fileUtils');
+const { writeTemplateLocally, exists } = require('./../util/fileUtils');
 const { getTemplate } = require('./../util/createLocalLambda');
 const namiLog = require('./../util/logger');
 
-module.exports = async function create(resourceName, homedir) {
-  let preLambdaName = `${resourceName}PreLambda`;
-  let postLambdaName = `${resourceName}PostLambda`;
+module.exports = async function create(resourceName) {
+  const preLambdaName = `${resourceName}PreLambda`;
+  const postLambdaName = `${resourceName}PostLambda`;
 
-  let preLambdaTemplate = await getTemplate('preLambda', false, resourceName);
-  let postLambdaTemplate = await getTemplate('postLambda', false, resourceName);
+  const preLambdaTemplate = await getTemplate('preLambda', false, resourceName);
+  const postLambdaTemplate = await getTemplate('postLambda', false, resourceName);
 
-  await writeTemplateLocally(preLambdaName, preLambdaTemplate);
-  await writeTemplateLocally(postLambdaName, postLambdaTemplate);
-  namiLog(`Your lambda templates, ${preLambdaName} and ${postLambdaName} are in ${process.cwd()}`);
-  namiLog('Please make any modifications to them before calling deploy');
+  const preLambdaDirectoryExists = await exists(`${process.cwd()}/${preLambdaName}`);
+  const postLambdaDirectoryExists = await exists(`${process.cwd()}/${postLambdaName}`);
+
+  if (!preLambdaDirectoryExists) {
+    await writeTemplateLocally(preLambdaName, preLambdaTemplate);
+    namiLog(`Your lambda template ${preLambdaName} is in ${process.cwd()}`);
+  } else {
+    namiLog(`Directory ${preLambdaName} already exists`);
+  }
+
+  if (!postLambdaDirectoryExists) {
+    await writeTemplateLocally(postLambdaName, postLambdaTemplate);
+    namiLog(`Your lambda template ${postLambdaName} is in ${process.cwd()}`);
+  } else {
+    namiLog(`Directory ${postLambdaName} already exists`);
+  }
+
+  namiLog('Please make any modifications to your handler files before calling deploy');
 };
