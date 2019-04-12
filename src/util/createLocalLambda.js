1380f6612586049c26ca192d5d6897cc3bb21444
Nick Miller <nick.miller00@gmail.com>
2019-04-12 09:37:20 -0700

    Refactoring
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/util/createLocalLambda.js
+++ src/util/createLocalLambda.js
@@ -1,20 +1,13 @@
-const fs = require('fs');
-const AWS = require('aws-sdk');
 const os = require('os');
 const getRegion = require('../util/getRegion');
-const region = getRegion();
 
-const apiVersion = 'latest';
+const region = getRegion();
 const homedir = os.homedir();
 
 const {
   readConfig,
   readFile,
-  writeFile,
-  copyFile,
-  mkdir,
   exists,
-  getNamiPath,
   writeTemplateToStage,
 } = require('./fileUtils');
 
@@ -24,7 +17,7 @@ const getTemplate = async (templateType, isCustomTemplate, resourceName) => {
   let lambdaTemplateLocation;
 
   if (isCustomTemplate) {
-    const type = `${templateType.slice(0,1).toUpperCase().concat(templateType.slice(1))}`;
+    const type = `${templateType.slice(0, 1).toUpperCase().concat(templateType.slice(1))}`;
     lambdaTemplateLocation = `${process.cwd()}/${resourceName}${type}/${resourceName}${type}.js`;
   } else {
     lambdaTemplateLocation = `${__dirname}/../../templates/${templateType}Template.js`;
@@ -32,36 +25,25 @@ const getTemplate = async (templateType, isCustomTemplate, resourceName) => {
 
   const lambdaTemplate = await readFile(lambdaTemplateLocation, 'utf8');
   return lambdaTemplate;
-}
+};
 
-const replaceStringPreLambda = async function(template, resourceName) {
+const replaceStringPreLambda = async (template, resourceName) => {
   const { accountNumber } = await readConfig(homedir);
   const queueURL = `https://sqs.${region}.amazonaws.com/${accountNumber}/${resourceName}SQS`;
   const lambdaTemplateWithRegion = template.replace('userRegion', region);
   const lambdaTemplateWithQueueURL = lambdaTemplateWithRegion.replace('queueURL', queueURL);
-
   return lambdaTemplateWithQueueURL;
-}
+};
 
-const replacePrivateIPPostLambda = async function (template, instanceId) {
+const replacePrivateIPPostLambda = async (template, instanceId) => {
   const privateIp = await getEC2PrivateIp(instanceId);
   return template.replace('privateIp', privateIp);
-}
-
-const generatePreLambdaTemplate = async function() {
-  const template = await getTemplate('preLambda');
-  return replaceStringPreLambda(template);
 };
 
-const generatePostLambdaTemplate = async function() {
-  const template = await getTemplate('preLambda');
-  return replacePrivateIPPostLambda(template);
-};
-
-async function createLocalLambda(resourceName, lambdaName, templateType, instanceId) {
-  let type = templateType.slice(0,1).toUpperCase().concat(templateType.slice(1));
+const createLocalLambda = async (resourceName, lambdaName, templateType, instanceId) => {
+  const type = templateType.slice(0, 1).toUpperCase().concat(templateType.slice(1));
   const path = `${process.cwd()}/${resourceName}${type}`;
-  let isCustomTemplate = await exists(path);
+  const isCustomTemplate = await exists(path);
   let updatedTemplate;
 
   const template = await getTemplate(templateType, isCustomTemplate, resourceName);
@@ -78,4 +60,4 @@ async function createLocalLambda(resourceName, lambdaName, templateType, instanc
 module.exports = {
   createLocalLambda,
   getTemplate,
-}
+};
