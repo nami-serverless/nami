56ad84b240019fbf76da5794f41df93fbc3f7f59
Sachin Chandy <sachinmchandy@gmail.com>
2019-04-19 17:26:26 +0100

    tests for local zip file and post lambda on aws
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- test/deployPostLambda.test.js
+++ test/deployPostLambda.test.js
@@ -1,39 +1,49 @@
-const deployPostLambda = require('../src/aws/deployPostLambda');
-const deployEC2 = require('../src/aws/deployEC2');
-const deploySecurityGroup = require('../src/aws/deploySecurityGroup');
-
-const { doesLambdaExist } = require('../src/aws/doesResourceExist');
-
 const os = require('os');
 
-const resourceName = 'testing';
-const securityGroupType = 'lambda';
+const deployEC2 = require('../src/aws/deployEC2');
+const deployPostLambda = require('../src/aws/deployPostLambda');
+const deploySecurityGroup = require('../src/aws/deploySecurityGroup');
+//const deployDLQ = require('../src/aws/deployDLQ');
+//const deploySQS = require('../src/aws/deploySQS');
+const { doesLambdaExist } = require('../src/aws/doesResourceExist');
+const destroy = require('../src/commands/destroy');
+
+const {
+  getStagingPath,
+  exists,
+  readConfig,
+} = require('../src/util/fileUtils');
+
+const resourceName = 'testNami1005';
+const postLambdaName = `${resourceName}PostLambda`;
+//const sqsName = `${resourceName}SQS`;
 const homedir = os.homedir();
-let instanceId;
-let lambdaType;
-let SecurityGroupId;
+const stagingPath = getStagingPath(homedir);
 
+// don't forget to deploy Q!
 describe('Nami deploy Post Lambda', () => {
-  jest.setTimeout(30000);
+  let instanceId;
+  let securityGroupId;
+
+  jest.setTimeout(60000);
   beforeAll(async () => {
+    jest.setTimeout(15000);
+    securityGroupId = await deploySecurityGroup(resourceName, 'lambda');
     instanceId = await deployEC2(resourceName, homedir);
-    SecurityGroupId = await deploySecurityGroup(resourceName, securityGroupType);
-    jest.setTimeout(30000);
-    await deployPostLambda(resourceName, homedir, instanceId, SecurityGroupId);
+    await deployPostLambda(resourceName, homedir, instanceId, securityGroupId);
   });
 
-  afterAll(async() => {
-
+  afterAll(async () => {
+    await destroy(resourceName, homedir);
   });
 
-  test('Lambda can connect to database', async() => {
-    // create ec2 instance with mongodb
-    // connect to mongodb
+  test(`Zip file exists within ${stagingPath}/${resourceName}`, async () => {
+    const zipFile = await exists(`${stagingPath}/${postLambdaName}/${postLambdaName}.zip`);
+    expect(zipFile).toBe(true);
   });
 
-  test.only('Lambda exists on AWS', async() => {
-    lambdaType = 'PostLambda';
-    const lambda = await doesLambdaExist(resourceName, lambdaType);
-    expect(lambda).toBe(true);
+  test('Lambda exists on AWS', async () => {
+    const functionExists = await doesLambdaExist(postLambdaName);
+    expect(functionExists).toBe(true);
   });
 });
