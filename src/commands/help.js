ff6d9264fe4f1a033808b021be9bf8bbb160250c
Sachin Chandy <sachinmchandy@gmail.com>
2019-04-24 20:54:35 +0100

    updated help output to stay within terminal width
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,7 +3,7 @@ const { log } = require('console');
 module.exports = () => {
   log('Commands:');
   log('\tcreate [resourceName]\tcreates local lambda templates for modification');
-  log('\tdeploy [resourceName]\tdeploys API endpoint and scalable architecture on AWS');
-  log('\tdestroy [resourceName]\tdeletes API endpoint and scalable architecture from AWS');
+  log('\tdeploy [resourceName]\tdeploys API endpoint');
+  log('\tdestroy [resourceName]\tdeletes API endpoint');
   log('\tlist\t\t\tlists API endpoints for all resources');
 };
