4fc827d7d9a094f2147eca36bb212ec44c85eca3
Sachin Chandy <sachinmchandy@gmail.com>
2019-04-24 20:42:42 +0100

    added logic to check if resource file exists
    Co-authored-by: Sachin Chandy <sachinmchandy@gmail.com>
    Co-authored-by: Nick Miller <nick.miller00@gmail.com>
    Co-authored-by: Wendy Kuhn <wendy@wendykuhn.io>

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,4 +1,4 @@
-const { readResources } = require('../util/fileUtils');
+const { readResources, getNamiPath, exists } = require('../util/fileUtils');
 const { asyncGetResources } = require('../aws/awsFunctions');
 const getRegion = require('../util/getRegion');
 
@@ -9,7 +9,15 @@ module.exports = async function list(homedir) {
   const noActiveEndpointError = 'You have no current active endpoints';
 
   try {
-    const { restApiId } = await readResources(homedir);
+    let restApiId;
+    const namiPath = getNamiPath(homedir);
+    const resourcePath = `${namiPath}/resources.json`;
+
+    if (await exists(resourcePath)) {
+      ({ restApiId } = await readResources(homedir));
+    } else {
+      throw new Error('Resources do not exist. Please run the deploy command first');
+    }
 
     const getResourcesParams = {
       restApiId,
